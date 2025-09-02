c8636cc1259f0bacaae385496e53c635d772223e
Gihaluf <gihaluf.bojar@gmail.com>
2025-09-02 12:02:09 +0300

    Izveidota metode kauliņa mešanai

--- src/lvt/Programma.java
+++ src/lvt/Programma.java
@@ -3,12 +3,13 @@ package lvt;
 import java.util.Random;
 
 public class Programma {
-
-	public static void main(String[] args) {
-		int skaitlis;
+	static void mestKaulinu() {
 		Random rand = new Random();
-		skaitlis = rand.nextInt(6)+1;
-		System.out.println("Uzkrita skaitlis: "+skaitlis);
+		int skaitlis = rand.nextInt(6)+1;
+		System.out.print("Uzkrita skaitlis: "+skaitlis);
+	}
+	public static void main(String[] args) {
+		mestKaulinu();
 	}
 
 }
